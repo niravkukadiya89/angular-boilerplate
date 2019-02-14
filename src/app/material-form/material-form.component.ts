7760f96b43a00e5ce74943c23d74259bf2c3bd8e
Krupa Khirasariya <krupa@ifourtechnolab.com>
2019-02-14 19:06:12 +0530

    form controls

--- src/app/material-form/material-form.component.ts
+++ src/app/material-form/material-form.component.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+
+export interface User {
+  name: string;
+}
+
+export interface Pokemon {
+  value: string;
+  viewValue: string;
+}
+
+export interface PokemonGroup {
+  disabled?: boolean;
+  name: string;
+  pokemon: Pokemon[];
+}
 
 @Component({
   selector: 'app-material-form',
@@ -7,11 +24,101 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./material-form.component.scss']
 })
 export class MaterialFormComponent implements OnInit {
-  // myControl = new FormControl();
-  // options: string[] = ['One', 'Two', 'Three'];
+  hide = true;
+  myControl = new FormControl();
+  pokemonControl = new FormControl();
+  toppings = new FormControl();
+  email = new FormControl('', [Validators.required, Validators.email]);
+  options: User[] = [
+    { name: 'Mary' },
+    { name: 'Shelley' },
+    { name: 'Ujjwal' },
+    { name: 'Prakash' },
+    { name: 'Nirav' },
+    { name: 'Igor' }
+  ];
+  filteredOptions: Observable<User[]>;
+  // ======================================================================
+  pokemonGroups: PokemonGroup[] = [
+    {
+      name: 'Grass',
+      pokemon: [
+        { value: 'bulbasaur-0', viewValue: 'Bulbasaur' },
+        { value: 'oddish-1', viewValue: 'Oddish' },
+        { value: 'bellsprout-2', viewValue: 'Bellsprout' }
+      ]
+    },
+    {
+      name: 'Water',
+      pokemon: [
+        { value: 'squirtle-3', viewValue: 'Squirtle' },
+        { value: 'psyduck-4', viewValue: 'Psyduck' },
+        { value: 'horsea-5', viewValue: 'Horsea' }
+      ]
+    },
+    {
+      name: 'Fire',
+      disabled: true,
+      pokemon: [
+        { value: 'charmander-6', viewValue: 'Charmander' },
+        { value: 'vulpix-7', viewValue: 'Vulpix' },
+        { value: 'flareon-8', viewValue: 'Flareon' }
+      ]
+    },
+    {
+      name: 'Psychic',
+      pokemon: [
+        { value: 'mew-9', viewValue: 'Mew' },
+        { value: 'mewtwo-10', viewValue: 'Mewtwo' },
+      ]
+    }
+  ];
+  // ======================================================================
+
+  // ==========================================================
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  // ========================================================
   constructor() { }
 
   ngOnInit() {
+    // ================For autocomplete start===========================
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith<string | User>(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter(name) : this.options.slice())
+      );
+    // ================For autocomplete end===========================
   }
 
+  // ================For autocomplete start===========================
+  displayFn(user?: User): string | undefined {
+    return user ? user.name : undefined;
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+    return this.options.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0);
+  }
+  // ================For autocomplete end===========================
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+
+  // =======================For slider start============================
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 0;
+    }
+
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
+  // ====================================================================
 }
--- src/app/material-form/material-form.module.ts
+++ src/app/material-form/material-form.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MaterialFormRoutingModule } from './material-form-routing.module';
 import { MaterialFormComponent } from './material-form.component';
 import { ShareModule } from '../shared/share.module';
+import { MatAutocompleteModule, MatSliderModule } from '@angular/material';
 
 @NgModule({
   declarations: [MaterialFormComponent],
   imports: [
     CommonModule,
     ShareModule,
-    MaterialFormRoutingModule
+    MaterialFormRoutingModule,
+    MatAutocompleteModule,
+    MatSliderModule
   ]
 })
 export class MaterialFormModule { }
